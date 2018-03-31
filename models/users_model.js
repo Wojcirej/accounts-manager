01b469e232531d6ccb2b50df028ae217ec6cebf4
Wojciech Bal <wojcirej@gmail.com>
2018-03-31 18:33:01 +0200

    Users model.

---
+++ models/users_model.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose'), Schema = mongoose.schema;
+var UserSchema = new Schema({
+  username: { type: String, unique: true },
+  email: String,
+  color: String,
+  hashed_password: String
+});
+mongoose.model('User', UserSchema);
