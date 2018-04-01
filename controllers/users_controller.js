5881ec46fe4e4790543dbc347321f1422b4158d4
Wojciech Bal <wojcirej@gmail.com>
2018-04-02 01:42:02 +0200

    Login component - view, controller, routing.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,28 @@ User = mongoose.model('User');
 function hashPW(pwd) {
   return crypto.createHash('sha256').update(pwd).digest('base64').toString();
 };
+
+exports.login = function(req, res) {
+  User.findOne({ username: req.body.username }).exec(function(error, user) {
+    if(!user) {
+      error = "User not found";
+    }
+    else if(user.hashed_password === hashPW(req.body.password.toString())) {
+      req.session.regenerate(function() {
+        req.session.user = user.id;
+        req.session.username = user.username;
+        req.session.msg = "Signed in as " + user.username;
+        res.redirect('/');
+      });
+    }
+    else {
+      error = "Authentication failed!";
+    }
+    if(error) {
+      req.session.regenerate(function() {
+        req.session.msg = error;
+        res.redirect('/login');
+      });
+    }
+  });
+};
--- routes.js
+++ routes.js
@@ -5,7 +5,8 @@ module.exports = function(app) {
   app.use('/static', express.static('./static')).
   use('/lib', express.static('../lib')
   );
-  app.get('/', function(req, res){
+
+  app.get('/', function(req, res) {
     if (req.session.user) {
       res.render('index', { username: req.session.username, msg: req.session.msg });
     }
@@ -13,5 +14,12 @@ module.exports = function(app) {
       req.session.msg = 'Access denied!';
       res.redirect('/login');
     }
+  });
+
+  app.get('/login', function(req, res) {
+    if(req.session.user) {
+      res.redirect('/');
+    };
+    res.render('login', { msg: req.session.msg} );
   });
 }
